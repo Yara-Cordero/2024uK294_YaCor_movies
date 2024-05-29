5d57d74a3b12f053bc971f947cd66c565be3a1df
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2024-05-29 11:34:47 +0200

    added other movieservices

--- movies-frontend/src/service/MovieService.ts
+++ movies-frontend/src/service/MovieService.ts
@@ -11,7 +11,39 @@ const MovieService = (api: AxiosInstance = baseInstance) => ({
     getMovieById: async(id: string) => {
         const data = await api.get(`/movies/${id}`);
         return data.data;
+    },
+    deleteMoviebyId: async(id: string) => {
+        try {
+            const response = await api.delete(`/movies/${id}`);
+            console.log("Movie deleted.")
+            return response.data;
+        } catch (error) {
+            console.error("error on delete.", error);
+            throw error;
+        }
+    },
+    addMovie: async (title : string, director: string, releaseDate : string) => {
+        try {
+             const data = {title, director, releaseDate};
+             const response = await api.post("/movies", data);
+             console.log("Movie created.")
+             return response.data;
+        }catch (error) {
+            console.log("error on create/put.", error);
+            throw error;
+        }
+    },
+    updateMovie: async (id: string, title: string, director: string, releaseDate: string) => {
+        try {
+            const data = {title, director, releaseDate};
+            const response = await api.put(`/movies/${id}`, data); 
+            console.log("Movie updated.")
+            return response.data; 
+        } catch (error) {
+            console.error("error on update", error);
+            throw error;
+        }
     }
-})
+});
 
 export default MovieService;
